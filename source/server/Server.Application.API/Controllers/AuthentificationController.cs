744a1bf622f17c183bf3230efa53670a2d687557
FUMOBOMBER <106705965+junkofuruto@users.noreply.github.com>
2023-06-07 20:49:36 +0300

    addition: created auth controller

---
+++ source/server/Server.Application.API/Controllers/AuthentificationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Presentation.API.Controllers
+{
+    public class AuthentificationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

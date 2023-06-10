137650881d011e4d32cd17fe7513a2eac40cfa1e
FUMOBOMBER <106705965+junkofuruto@users.noreply.github.com>
2023-06-10 20:18:46 +0300

    addition: added props to login resp

--- source/server/Server.Presentation.Contracts/Authentification/LoginResponse.cs
+++ source/server/Server.Presentation.Contracts/Authentification/LoginResponse.cs
@@ -2,5 +2,11 @@
 {
     public sealed class LoginResponse
     {
+        public string Id { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public string Token { get; set; } = null!;
+
+        // TODO:
+        // * specify additional field for response
     }
 }

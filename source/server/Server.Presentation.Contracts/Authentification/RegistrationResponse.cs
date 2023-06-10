8053f012d9453d71c61d4b85a86d378d0a13e347
FUMOBOMBER <106705965+junkofuruto@users.noreply.github.com>
2023-06-10 20:17:41 +0300

    addition: added basic props to registration response
    {
        "id": <string>,
        "username": <string>,
        "token": <string>
    }

--- source/server/Server.Presentation.Contracts/Authentification/RegistrationResponse.cs
+++ source/server/Server.Presentation.Contracts/Authentification/RegistrationResponse.cs
@@ -2,5 +2,11 @@
 {
     public sealed class RegistrationResponse
     {
+        public string Id { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public string Token { get; set; } = null!;
+
+        // TODO:
+        // * specify additional response field
     }
 }

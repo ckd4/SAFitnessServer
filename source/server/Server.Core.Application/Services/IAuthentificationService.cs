ed821ec3f5e752b7372d5fa519a7b7114ddf8805
FUMOBOMBER <106705965+junkofuruto@users.noreply.github.com>
2023-07-09 22:07:35 +0300

    cleaning stuff

--- source/server/Server.Core.Application/Services/IAuthentificationService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Server.Core.Application.Services
-{
-    public interface IAuthentificationService
-    {
-        Task Login(string username, string password);
-        Task Register(string username, string password, string firstName, string lastName);
-        Task GetUserData(string token);
-    }
-}
--- source/server/Server.Presentation.Contracts/Authentification/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Server.Presentation.Contracts.Authentification
-{
-    public sealed class LoginRequest
-    {
-        public string Username { get; set; } = null!;
-        public string Password { get; set; } = null!;
-    }
-}

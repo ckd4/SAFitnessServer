d28fed839d0869352b713410ce8cde9488448b1a
FUMOBOMBER <106705965+junkofuruto@users.noreply.github.com>
2023-07-09 22:38:59 +0300

    projects init

---
+++ LoggerService/LogType.cs
@@ -0,0 +1,11 @@
+﻿namespace LoggerService
+{
+    public enum LogType
+    {
+        Trace = ConsoleColor.White,
+        Debug = ConsoleColor.Cyan,
+        Info = ConsoleColor.Green,
+        Warning = ConsoleColor.Yellow,
+        Error = ConsoleColor.Red,
+    }
+}
